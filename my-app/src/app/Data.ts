9b54da71fe3afd84d951c79e27b1a44168382df3
piyushayu <piyush.ps1412@outlook.com>
2026-08-31 00:34:04 +0530

    writethedatainsidethedatafileforportfolio

--- my-app/src/app/Data.ts
+++ my-app/src/app/Data.ts
@@ -1,67 +1,85 @@
+import { Dataschema } from "./DataSchema";
 
-export interface Data extends Document {
-  home : Object;
-  projects : Object[];
-  tech : Object;
-  Contact : Object;
-  footer : Object;
-}
+export const Data : Dataschema = {
+    home : {
+        name : "Piyush",
+        description : ["I'm a web developer" , "I'm a software engineer"],
+        avatar : "",
+        homeimage : "",
+        Role : ["Full Stack Developer", "A Frontend Developer" , "A Backend Developer"],
+        Sociallink : [
+        {
+            platform : "Twitter",
+            url : "https://twitter.com/piyush",
+            icon : "",
+        },
+        {
+            platform : "Github",
+            url : "https://github.com/piyuayu",
+            icon : "",
+        },
+        {
+            platform : "Linkedin",
+            url : "www.linkedin.com/in/piyush-singh-1b9a0036a",
+            icon : "",
+        }
+        ]
+    },
 
-export interface link {
- platform : String,
- url : String,
- icon : String,
-}
+    projects : [
+        {
+            title : "Vetshaathi",
+            shortdescription : "A virtual consultant for animal disease",
+            description : "It helps to find the disease on the basis of suitable symptoms and give the medicine details to cure it",
+            image : "",
+            link : "",
+            githublink : "",
+            tech : [
+                {
+                    name : "React"
+                },
+                {
+                    name : "Supabase"
+                },
+                {
+                    name : "Tailwind"
+                }
+            ]
+        }
+    ],
 
-export interface Stack {
-    name : String,
-    image : String
-}
+    tech : {
+        Frontend: [
+            { name: "HTML", image: "" },
+            { name: "CSS", image: "" },
+            { name: "JavaScript", image: "" },
+            { name: "TypeScript", image: "" },
+            { name: "React", image: "" },
+            { name: "Next.js", image: "" },
+            { name: "Tailwind CSS", image: "" },
+            { name: "Redux", image: "" },
+          ],
+          Backend: [
+            { name: "Node.js", image: "" },
+            { name: "Express", image: "" },
+            { name: "MongoDB", image: "" },
+          ],
+          Stack: [
+            { name: "VS Code", image: "" },
+            { name: "Git", image: "" },
+          ],
+    },
 
-export interface projecttech {
-    name : string
-}
+    Contact : {
+        Headline : "Wanna Share your thoughts with me"
+    },
 
-export interface Dataschema extends Data {
-   home : {
-    name : String,
-    description : String,
-    avatar : String,
-    homeimage : String,
-    Role : String[],
-    Sociallink : link[];
-   },
-    
-   projects : [
-    {
-    title : String,
-    shortdescription : String,
-    description : String,
-    image : String,
-    link : String,
-    githublink : String,
-    tech : projecttech[]
+    footer :{
+        Quote : "Your every thought becomes quote when you become successfull",
+        Quotename : "Piyush Singh",
+        name : "Piyush",
+        year : 2026
     }
-   ],
 
-   tech : {
-    Frontend : Stack[],
-    Backend : Stack[],
-    Stack : Stack[];
-   },
-
-   Contact : {
-    Headline : String,
-   }
-
-   footer : {
-    Quote : String,
-    name : String,
-    year : Number;
-   }
-
-} 
-
-
-
+}
 
---
+++ my-app/src/app/DataSchema.ts
@@ -0,0 +1,68 @@
+
+export interface Data extends Document {
+  home : Object;
+  projects : Object[];
+  tech : Object;
+  Contact : Object;
+  footer : Object;
+}
+
+export interface link {
+ platform : String,
+ url : String,
+ icon : String,
+}
+
+export interface Stack {
+    name : String,
+    image : String
+}
+
+export interface projecttech {
+    name : string
+}
+
+export interface Dataschema {
+   home : {
+    name : String,
+    description : String[],
+    avatar : String,
+    homeimage : String,
+    Role : String[],
+    Sociallink : link[];
+   },
+    
+   projects : [
+    {
+    title : String,
+    shortdescription : String,
+    description : String,
+    image : String,
+    link : String,
+    githublink : String,
+    tech : projecttech[]
+    }
+   ],
+
+   tech : {
+    Frontend : Stack[],
+    Backend : Stack[],
+    Stack : Stack[];
+   },
+
+   Contact : {
+    Headline : String,
+   }
+
+   footer : {
+    Quote : String,
+    Quotename : String,
+    name : String,
+    year : Number;
+   }
+
+} 
+
+
+
+
